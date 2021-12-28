c05fa8fa7cef3ed24f38bb0e137433f25c0ab790
dmitry <rozanovdm@gmail.com>
2021-12-28 22:19:09 +0300

    homework_3

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:import_sorter/args.dart';
+import 'package:import_sorter/files.dart';
+import 'package:import_sorter/sort.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+            child: CarouselSlider(
+          options: CarouselOptions(height: 400.0),
+          items: [
+            'assets/images/bzr.svg',
+            'assets/images/android1.svg'
+          ].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: EdgeInsets.symmetric(horizontal: 5.0),
+                    child: SvgPicture.asset('$i'));
+              },
+            );
+          }).toList(),
+        )
+        )
+    );
+  }
+}
